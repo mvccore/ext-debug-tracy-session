d86c656f099d4e01a77cf664f919c1d5826b22c6
Tom Flídr <tomflidr@gmail.com>
2021-07-30 15:06:02 +0200

    release v5.0.3

--- src/MvcCore/Ext/Debugs/Tracys/SessionPanel.php
+++ src/MvcCore/Ext/Debugs/Tracys/SessionPanel.php
@@ -24,7 +24,7 @@ class SessionPanel implements \Tracy\IBarPanel {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.2';
+	const VERSION = '5.0.3';
 
 	/**
 	 * Internal constants to recognize session record types in template.
